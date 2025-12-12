c64c3e21beb06c5fd222737b5930bea2a09c599a
saitam93 <saitam93@gmail.com>
2025-12-12 13:16:35 -0300

    fix: agregar dependencias faltantes openmeteo y js-eval

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -4,7 +4,7 @@ from logic.inference import get_recommendation
 from ui.feedback import render_feedback_section
 
 def render_dashboard():
-    st.title("WeatherWear 🧣")
+    st.title("IsiWear 🧣")
     
     # 1. Obtener Datos (Services)
     with st.spinner("Consultando satélites..."):
